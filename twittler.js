3fc21129ebda165fdefc5d6b13d8c8098b6da642
Ephraim Glick <ephraim.glick@gmail.com>
2017-08-01 19:11:20 -0700

    provide basic and advanced functionality with styling

--- twittler.js
+++ twittler.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
-  var $tweetBox = $('body').find(".tweetBox");
+  
+  // initialize variable for tracking which tweets are old/new
   var prevHomeLength = streams.home.length;
 
+  // helper functions
+
+  // render tweets on page
   var displayTweet = function(tweet) {
     var $tweet = $('<div class="tweet"></div>');
+    $tweet.data("date", moment(tweet.created_at));
+    $tweet.data("user", tweet.user);
     var $user = $('<div class="tweetUser user"></div>');
     $user.text('@' + tweet.user);
     $user.appendTo($tweet);
@@ -11,55 +17,93 @@ $(document).ready(function(){
     $message.text(tweet.message);
     $message.appendTo($tweet);
     var $date = $('<div class="tweetDate"></div>');
-    $date.text(tweet.created_at);
+    $date.text(moment(tweet.created_at).fromNow());
     $date.appendTo($tweet);
+    var $tweetBox = $('body').find(".tweetBox");
     $tweet.prependTo($tweetBox);
   }
 
-  // Populate page with initial tweets
-  var index = streams.home.length - 1;
-  while(index >= 0){
-    var tweet = streams.home[index];
-    displayTweet(tweet);
-    index -= 1;
+  // animate box-shadow on button press
+  var animateButtonPress = function(jQueryBtn) {
+    jQueryBtn.animate({boxShadow: '1px 1px 1px', top: '2'}, 100, function(){
+    jQueryBtn.animate({boxShadow: '0 10px 6px -6px #777', top: '0'});
+      });
   }
 
-  // Display all tweets on click
-      // why can't I separate out this function?
-  $('.refreshFeed').on('click', function () {
-    for (let t = 0; t < streams.home.length; t++) {
+  var showNewTweets = function() {
+    for (let t = prevHomeLength; t < streams.home.length; t++) {
       var tweet = streams.home[t];
       displayTweet(tweet);
-      $(tweet).show();
     }
+    prevHomeLength = streams.home.length;
+  }
+
+  var updateTimestamps = function () {
+    $('.tweet').each(function(){
+      let newTimestamp = $(this).data("date").fromNow()
+      $(this).find('.tweetDate').text(newTimestamp);
+    })
+  }
+
+  // populate page with initial tweets
+  var index = 0;
+  while(index <= streams.home.length - 1){
+    var tweet = streams.home[index];
+    displayTweet(tweet);
+    index += 1;
+  }
+
+
+  // populate "following" list
+  for (let i = 0; i < window.users.length; i++) {
+    var $followedPar = $('<p class="user"></p>');
+    $followedPar.text('@' + window.users[i]);
+    $followedPar.appendTo($('.following'));
+  }
+
+
+  // "refresh" button action
+  $('.refreshFeed').on('click', function () {
+    animateButtonPress($(this));
+    // remember the scroll position
+    var old_scroll = $(window).scrollTop(); 
+    showNewTweets();
+    updateTimestamps();
+    //restore scroll position
+    $(document).scrollTop(old_scroll); 
   });
 
 
-  // Post new tweet on click
-  $('.userTweetButton').on('click', function () {
+  // "post" button actions
+  $('.userTweetButton').on('click', function (event) {
+    animateButtonPress($(this));
+    // make sure you have a stream
     if (!window.users.includes("your_username")) {
       streams.users.your_username = [];
     }
+    // construct your tweet, add to stream
     var newUserTweet = {};
     newUserTweet.user = "your_username";
     newUserTweet.message = $('.userTweetInput').val();
-    newUserTweet.created_at = new Date();
+    newUserTweet.created_at = moment();
     streams.users.your_username.push(newUserTweet);
     streams.home.push(newUserTweet);
     $('.userTweetInput').val('');
   });     
 
-  // Look at one user's feed on click
+
+  // show a user's feed on click
   $('*').on('click', '.user', function() {
     var theUser = $(this).text().trim();
-    // if (theUser[0] = '@') { 
-    //   theUser = theUser.slice(1) 
-    // };
     $('.tweet').hide();
     $('.tweet').filter(function() {
       return $(this).find('.user').text() === theUser;
     }).show();
-   });
+  });
+
+
+
+
 
 
 });
